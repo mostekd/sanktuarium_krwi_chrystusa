50ce59a0e424cff5b2f05423e41a26c8497ad06f
Dawid Mostowski <dawid-mostowski@o2.pl>
2024-02-05 23:08:17 +0100

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const burger = document.querySelector('.burger');
-    const navLinks = document.querySelector('.nav-links');
-
-    burger.addEventListener('click', function () {
-        navLinks.classList.toggle('show');
-    });
-});
